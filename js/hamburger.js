bd7e7dc1a3eb813e9f30c3403e06b6cd08eeb353
Maciek <maciek@bartosik.wtf>
2019-11-20 11:37:07 +0100

    Nav hide on clientX

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,8 +2,17 @@ const hamburgerToggle = document.querySelector('.hamburger__toggle');
 const nav = document.querySelector('.nav');
 
 const showNav = () => {
-  hamburgerToggle.classList.toggle('hamburger__toggle--active');
-  nav.classList.toggle('nav--active');
+    hamburgerToggle.classList.toggle('hamburger__toggle--active');
+    nav.classList.toggle('nav--active');
 };
 
 hamburgerToggle.addEventListener('click', showNav);
+
+window.addEventListener('click', function () {
+
+    if (window.innerWidth < 750 && window.innerWidth - event.screenX < 100) {
+        showNav();
+    }
+});
+
+
