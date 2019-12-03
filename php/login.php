6a5c3db12c7862d6074689a14526d962a68dad21
Maciek <maciek@bartosik.wtf>
2019-12-03 20:31:45 +0100

    Small fix

--- php/login.php
+++ php/login.php
@@ -1,7 +1,7 @@
 <?php
 include "env.php";
 try{
-    $pdo = new PDO('mysql:host'.getenv("DB_HOST").';dbname='.getenv("DB_NAME"), getenv("DB_USERNAME"), getenv("DB_PASSWORD"));
+    $pdo = new PDO('mysql:host='.getenv("DB_HOST").';dbname='.getenv("DB_NAME"), getenv("DB_USERNAME"), getenv("DB_PASSWORD"));
     echo "Jest git";
 }catch (PDOException $e){
     echo "Nie jest git";
